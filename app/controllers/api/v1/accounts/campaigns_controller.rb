ed2873344b441881841b2c193b3c727a69524748
Taranbir Singh <taranbir.singh@gamechangesns.com>
2023-11-30 19:51:21 +0530

    chore(gc-woot): shop 4155

--- app/controllers/api/v1/accounts/campaigns_controller.rb
+++ app/controllers/api/v1/accounts/campaigns_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::Accounts::CampaignsController < Api::V1::Accounts::BaseController
   before_action :campaign, except: [:index, :create]
+  before_action :parse_audience, only: [:create]
   before_action :check_authorization
 
   def index
@@ -29,6 +30,18 @@ class Api::V1::Accounts::CampaignsController < Api::V1::Accounts::BaseController
 
   def campaign_params
     params.require(:campaign).permit(:title, :description, :message, :enabled, :trigger_only_during_business_hours, :inbox_id, :sender_id,
-                                     :scheduled_at, audience: [:type, :id], trigger_rules: {}, template: {})
+                                     :scheduled_at, audience: [], trigger_rules: {}, template: {})
+  end
+
+  def parse_audience
+    return unless params[:audience_type] && params[:audience_type] == 'contacts'
+
+    time = Time.now.to_i
+    contact_ids = params[:audience]
+    contact_ids.each do |contact_id|
+      contact = Current.account.contacts.find(contact_id)
+      contact.add_labels([time.to_s])
+    end
+    params[:campaign][:audience] = [time.to_s]
   end
 end
