8cefc0c75410296ab7119f32dbb620da5e143a48
sahilbhadana-gamechange <sahil.bhadana@gamechangesns.com>
2023-12-01 11:44:34 +0530

    feat: get customer keys api

---
+++ app/controllers/api/v1/accounts/customer_keys_controller.rb
@@ -0,0 +1,41 @@
+class Api::V1::Accounts::CustomerKeysController < Api::V1::Accounts::CustomAttributeDefinitionsController
+  def index
+    additional_attributes = additional_custom_attributes
+    filtered_attributes = map_custom_attributes(@custom_attribute_definitions)
+    filtered_attributes += additional_attributes
+
+    render json: { data: filtered_attributes }
+  end
+
+  private
+
+  def map_custom_attributes(objects)
+    objects.map do |object|
+      {
+        'DisplayName': object['attribute_display_name'],
+        'type': object['attribute_display_type'],
+        'keyName': object['attribute_key']
+      }
+    end
+  end
+
+  def additional_custom_attributes
+    [
+      {
+        DisplayName: 'Name',
+        keyName: 'name',
+        type: 'text'
+      },
+      {
+        DisplayName: 'Email',
+        keyName: 'email',
+        type: 'text'
+      },
+      {
+        DisplayName: 'Phone Number',
+        keyName: 'phone_number',
+        type: 'text'
+      }
+    ]
+  end
+end
