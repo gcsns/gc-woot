9951554ea39134070aa27b72921f544f4950b674
fawazse <fawaz.sullia@shopkey.com>
2023-12-01 14:38:13 +0400

    fix: review changes and migration

--- app/models/message.rb
+++ app/models/message.rb
@@ -15,21 +15,25 @@
 #  created_at            :datetime         not null
 #  updated_at            :datetime         not null
 #  account_id            :integer          not null
-#  conversation_id       :integer          not null
+#  campaign_id           :bigint
+#  conversation_id       :integer
 #  inbox_id              :integer          not null
 #  sender_id             :bigint
 #  source_id             :string
 #
 # Indexes
 #
-#  index_messages_on_account_id                         (account_id)
-#  index_messages_on_account_id_and_inbox_id            (account_id,inbox_id)
-#  index_messages_on_additional_attributes_campaign_id  (((additional_attributes -> 'campaign_id'::text))) USING gin
-#  index_messages_on_content                            (content) USING gin
-#  index_messages_on_conversation_id                    (conversation_id)
-#  index_messages_on_inbox_id                           (inbox_id)
-#  index_messages_on_sender_type_and_sender_id          (sender_type,sender_id)
-#  index_messages_on_source_id                          (source_id)
+#  index_messages_on_account_id                 (account_id)
+#  index_messages_on_account_id_and_inbox_id    (account_id,inbox_id)
+#  index_messages_on_content                    (content)
+#  index_messages_on_conversation_id            (conversation_id)
+#  index_messages_on_inbox_id                   (inbox_id)
+#  index_messages_on_sender_type_and_sender_id  (sender_type,sender_id)
+#  index_messages_on_source_id                  (source_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (campaign_id => campaigns.id) ON DELETE => cascade
 #
 
 class Message < ApplicationRecord
@@ -91,7 +95,6 @@ class Message < ApplicationRecord
   belongs_to :account
   belongs_to :inbox
   belongs_to :conversation, touch: true
-  belongs_to :campaign
 
   # FIXME: phase out user and contact after 1.4 since the info is there in sender
   belongs_to :user, required: false
