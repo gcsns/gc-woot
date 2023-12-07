4f662a1038aba69a5e10dc1be6a0e88d4d878cfe
fawazse <fawaz.sullia@shopkey.com>
2023-12-07 16:06:14 +0400

    fix: fix review comments

--- app/controllers/api/v1/campaigns_controller.rb
+++ app/controllers/api/v1/campaigns_controller.rb
@@ -1,50 +1,31 @@
 class Api::V1::CampaignsController < ApplicationController
   def show
-    @id = params[:id]
-    @campaign = Campaign.find(@id)
-    audience_label_ids = @campaign.audience.pluck('id')
-    contact_counts = Contact.joins(:taggings).where(taggings: { tag_id: audience_label_ids }).distinct.count
-    formatted_campaign = {
-      id: @campaign.id,
-      title: @campaign.title,
-      message: @campaign.message,
-      campaign_type: @campaign.campaign_type,
-      campaign_status: @campaign.campaign_status,
-      audience: @campaign.audience,
-      scheduled_at: @campaign.scheduled_at,
-      sent_count: @campaign.sent_count,
-      template: @campaign.template,
-      delivered_count: @campaign.delivered_count,
-      failed_count: @campaign.failed_count,
-      clicked_count: @campaign.clicked_count,
-      read_count: @campaign.read_count,
-      replied_count: @campaign.replied_count,
-      audience_count: contact_counts
-    }
-    render json: formatted_campaign
+    id = params[:id]
+    campaign = Campaign.find(id)
+    render json: campaign
   rescue ActiveRecord::RecordNotFound
     render json: { error: 'Campaign not found' }, status: :not_found
   end
 
   def messages
-    @campaign_id = params[:id]
+    campaign_id = params[:id]
     limit = params[:limit]
     offset = params[:offset]
     status = params[:status]
-    @campaign = Campaign.find(@campaign_id)
-    messages = fetch_messages(@campaign_id, limit, offset, status)
-    formatted_messages = get_formatted_messages(messages, @campaign)
+    campaign = Campaign.find(campaign_id)
+    messages = fetch_messages(campaign_id, limit, offset, status)
+    formatted_messages = get_formatted_messages(messages, campaign)
     render json: formatted_messages
   end
 
   private
 
   def fetch_messages(id, limit, offset, status)
-    @messages = if status
-                  Message.where({ campaign_id: id, status: status }).limit(limit).offset(offset)
-                else
-                  Message.where(campaign_id: id).limit(limit).offset(offset)
-                end
+    messages = if status
+                 Message.where({ campaign_id: id, status: status }).limit(limit).offset(offset)
+               else
+                 Message.where(campaign_id: id).limit(limit).offset(offset)
+               end
   end
 
   def get_formatted_messages(messages, campaign)
--- app/javascript/dashboard/routes/dashboard/settings/campaigns/whatsapp-compaign/components/CampaignAnalytics.vue
+++ app/javascript/dashboard/routes/dashboard/settings/campaigns/whatsapp-compaign/components/CampaignAnalytics.vue
@@ -22,7 +22,7 @@ export default {
     const id = split[split.length - 1];
     const token =
       'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyLCJleHAiOjE3MzI4MjQwMDAwMDB9.q2UqaJswlXkpqi4xk1Grp7vXj1g9DE9OvyjH4HV2DbU';
-    const path = `http://localhost:3001/campaignDetails/${id}?hideSideBar=true?token=${token}`;
+    const path = `http://localhost:3001/campaignDetails/${id}?hideSideBar=true&token=${token}`;
     this.url = path;
   },
   methods: {},
